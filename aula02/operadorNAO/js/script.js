fa5f8533e7696e1598978b9256ada10017291ea6
Felipe Thiago C. C. Oliveira <felipethiagorx@gmail.com>
2022-01-31 21:39:40 -0300

    Atualização JavaScript Logica

---
+++ aula02/operadorNAO/js/script.js
@@ -0,0 +1,12 @@
+//Leia um login e se este não for o administrador exiba a mensagem: SEM PODERES ADMINISTRATIVOS
+
+let usuario=prompt("Usuário");
+
+if (!(usuario=="administrador")){
+
+    alert ("Sem poderes administrativos!");
+
+}else{
+    
+    alert ("Olá administrador!");
+}
