db61405b529d92237b4d78cf1242448a13a31f4b
Felipe Thiago C. C. Oliveira <98181566+felipethiago21@users.noreply.github.com>
2022-02-14 09:31:31 -0300

    Delete aula01 directory

--- aula01/calculoAutonomiaGasolina/js/script.js
+++
@@ -1,5 +0,0 @@
-let kmin=parseFloat(prompt("Digite a quantidade de KM ao iniciar a corrida"));
-let kmche=parseFloat(prompt("Digite a quantidade de KM ao chegar ao posto"));
-let gas=parseFloat(prompt("Digite a quantidade de litros reabastecido"));
-
-alert (`Média de KM/L: ${kmche-kmin/gas}`);
--- aula01/conversaoDollarReal/js/script.js
+++
@@ -1,7 +0,0 @@
-let doacao=parseFloat(prompt("Digite o valor da doação em dólares"));
-let valorDolar=parseFloat(prompt("Digite o valor da cotação do dólar"));
-let valorEmReais=doacao*valorDolar;
-
-alert (`O valor da doação em reais é R$:${valorEmReais} baseado na cotação atual de $${valorDolar}`);
-
-
