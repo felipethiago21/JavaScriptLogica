83d52a176063487b4d9c33acf4df5b4bec5e4eec
Felipe Thiago C. C. Oliveira <98181566+felipethiago21@users.noreply.github.com>
2022-02-14 09:31:24 -0300

    Delete aula04 directory

--- aula04/circulo/js/script.js
+++
@@ -1,6 +0,0 @@
-function calcular (){
-    let raio=parseFloat(document.getElementById("raio").value);
-    let area=(Math.PI*Math.pow(raio,2)).toFixed(2);
-    let comprimento=(2*Math.PI*raio).toFixed(2);
-    document.getElementById("resultado").innerHTML=`Area: ${area.toString()}<br>Comprimento: ${comprimento.toString()}`;
-}
